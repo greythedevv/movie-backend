98a93f3def906587b28e0d570fc173175960888f
Greatness <Oluwolegreatnessadeola@gmail.com>
2026-04-11 16:42:33 +0100

    create user

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,6 +1,43 @@
+
+import {prisma} from "../config/db.js"
+import bcrypt from "bcryptjs";
+
 const register = async(req, res)=>{
     const body = req.body;
-    res.json(body)
+    const {name, email, password} = body
+
+    // check if user already exist 
+    const userExist = await prisma.user.findUnique({
+        where:{email:email}
+    })
+
+    if (userExist){
+        return res.status(400).json({error: "user already exists with this email"})
+    }
+
+    // hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // create user
+    const user = await prisma.user.create({
+        data:{
+            name,
+            email,
+            password: hashedPassword,
+        }
+        
+    });
+
+    res.status(201).json({
+        status: "success",
+        data: {
+            user:{
+                id: user.id,
+                name:name,
+                email:email,
+        }
+    }})
 }
 
 
