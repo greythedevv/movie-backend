fcbe83e71e15b67062e9f33947bfaf6252bd9ec1
Greatness <Oluwolegreatnessadeola@gmail.com>
2026-04-11 14:14:22 +0100

    initial commit

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,10 @@
+import express from "express"
+import { register } from "../controllers/authControllers"
+
+const router = express.Router()
+
+router.post("/register", register )
+
+
+
+export default router
