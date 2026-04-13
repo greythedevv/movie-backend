75217e2fb86624be52a51879fc8d165563647512
Greatness <Oluwolegreatnessadeola@gmail.com>
2026-04-13 19:25:34 +0100

    added auth and movie validator

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,12 @@
 import express from "express"
 import { register, login, logout } from "../controllers/authControllers.js"
+import { loginSchema, registerSchema } from "../validators/authValidators.js"
+import { validateRequest } from "../middleware/validateRequest.js"
 
 const router = express.Router()
 
-router.post("/register", register )
-router.post("/login", login )
+router.post("/register", validateRequest(registerSchema),register )
+router.post("/login",validateRequest(loginSchema) ,login )
 router.post("/logout", logout)
 
 
